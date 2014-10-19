3e4e0b3ddb34c52389484bb13d17d11903eb11e2
Graham Higgins <gjh@bel-epa.com>
2014-10-19 07:44:53 +0100

    Diffplot feature

--- src/qt/walletview.h
+++ src/qt/walletview.h
@@ -104,6 +104,9 @@ public slots:
 
     void setEncryptionStatus();
 
+    /** Update the plot on the overview (home) page */
+    void updatePlot(int count);
+
 signals:
     /** Signal that we want to show the main window */
     void showNormalIfMinimized();
