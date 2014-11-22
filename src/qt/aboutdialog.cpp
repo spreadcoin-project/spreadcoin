b6128d4376c9a24fbfb4238edebb0f50baf1173f
Mr. Spread <Mr-Spread@users.noreply.github.com>
2014-11-22 20:44:49 +0300

    Remove unused defines

--- src/qt/aboutdialog.cpp
+++ src/qt/aboutdialog.cpp
@@ -4,10 +4,6 @@
 #include "clientmodel.h"
 #include "clientversion.h"
 
-// Copyright year (2009-this)
-// Todo: update this when changing our copyright comments in the source
-const int ABOUTDIALOG_COPYRIGHT_YEAR = 2014;
-
 AboutDialog::AboutDialog(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::AboutDialog)
